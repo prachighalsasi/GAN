9736b8bfa45a8e615402abdffd434aa71846657b
prachighalsasi <62062564+prachighalsasi@users.noreply.github.com>
2021-05-02 10:35:35 +0530

    Update model.py

--- model.py
+++ model.py
@@ -5,7 +5,7 @@ from keras.layers.core import Activation
 from keras.layers.advanced_activations import LeakyReLU
 from keras.layers.normalization import BatchNormalization
 from keras.layers.convolutional import UpSampling2D
-from keras.layers.convolutional import Convolution2D
+from keras.layers.convolutional import Conv2D
 from keras.layers import Input, LSTM, RepeatVector, Lambda
 from keras.layers.core import Flatten
 from keras.optimizers import Adam
@@ -27,34 +27,34 @@ def generator_model(inputdim = 100, xdim = 4, ydim = 4):
     model.add(Activation('relu'))
     model.add(Reshape( (1024, xdim, ydim), input_shape=(inputdim,) ) )
     model.add(UpSampling2D(size=(2, 2)))
-    model.add(Convolution2D(512, 5, 5, border_mode='same'))
+    model.add(Conv2D(512, 5, 5, border_mode='same'))
     model.add(BatchNormalization())
     model.add(Activation('relu'))
     model.add(UpSampling2D(size=(2, 2)))
-    model.add(Convolution2D(256, 5, 5, border_mode='same'))
+    model.add(Conv2D(256, 5, 5, border_mode='same'))
     model.add(BatchNormalization())
     model.add(Activation('relu'))
     model.add(UpSampling2D(size=(2, 2)))
-    model.add(Convolution2D(128, 5, 5, border_mode='same'))
+    model.add(Conv2D(128, 5, 5, border_mode='same'))
     model.add(BatchNormalization())
     model.add(Activation('relu'))
     model.add(UpSampling2D(size=(2, 2)))
-    model.add(Convolution2D(3, 5, 5, border_mode='same'))
+    model.add(Conv2D(3, 5, 5, border_mode='same'))
     model.add(Activation('tanh'))
     return model
 
 def discriminator_model():
     model = Sequential()
-    model.add(Convolution2D(128, 5, 5, pooling=(2, 2), input_shape=(3, 64, 64), border_mode = 'same'))
+    model.add(Conv2D(128, 5, 5, input_shape=(3, 64, 64), border_mode = 'same'))
     model.add(LeakyReLU(0.2))
     model.add(Dropout(0.2))
-    model.add(Convolution2D(256, 5, 5, pooling=(2, 2), border_mode = 'same'))
+    model.add(Conv2D(256, 5, 5, border_mode = 'same'))
     model.add(LeakyReLU(0.2))
     model.add(Dropout(0.2))
-    model.add(Convolution2D(512, 5, 5, pooling=(2, 2), border_mode = 'same'))
+    model.add(Conv2D(512, 5, 5, border_mode = 'same'))
     model.add(LeakyReLU(0.2))
     model.add(Dropout(0.2))
-    model.add(Convolution2D(1024, 5, 5, pooling=(2, 2), border_mode = 'same'))
+    model.add(Conv2D(1024, 5, 5, border_mode = 'same'))
     model.add(LeakyReLU(0.2))
     model.add(Dropout(0.2))
     model.add(Flatten())
